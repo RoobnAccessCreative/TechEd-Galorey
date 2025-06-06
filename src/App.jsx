5316dfcbc534c735cc12b660a0f08c2ad2855dfd
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-06-06 08:48:29 +0100

    made new files and boilerplate useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
 export default function App() {
+  const [imges, setImges] = useState([]);
+
+  useEffect(() => {
+    (async function getImages() {
+      const res = await fetch(import.meta.env.VITE_API);
+      const data = res.ok ? await res.json() : [];
+      setImges(data);
+    })();
+  }, []);
+
   return (
     <>
       <Navbar />
